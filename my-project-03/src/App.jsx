05aa245c4c71675cdb08a2b745adbe99d57af105
6519N10003 <s6519N10003@sau.ac.th>
2025-03-18 10:19:08 +0700

    version 0.0.3

---
+++ my-project-03/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import wow from './wow.jsx'
+
+
+function App() {
+  return (
+  <>
+  Welcome
+  </>
+  
+  )
+}
+  
+  function Hello(){
+  return(
+  <React. Fragment>
+  Hello............
+  <wow/>
+  <wow/>
+  <wow/>
+  
+
+  </React.Fragment>
+  )
+  }
+
+  function Hi(){
+  return(
+  <div>
+  
+  </div>
+  
+  )
+}
+  
+  export default App
